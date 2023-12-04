d7dfa67ada5ddb6ddf7444fa7a56c3227d634b23
kishanga <85829332+kishanga@users.noreply.github.com>
2023-12-04 20:29:40 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,7 +1,18 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
-
+import av
+import cv2
 
 st.title("Hello 2021!")
 
-webrtc_streamer(key="sample")
+class VideoProcessor:
+  def recv(self, frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+
+webrtc_streamer(key="edge", video_processor_factory=VideoProcessor())
