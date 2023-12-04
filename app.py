7f4b3984d41feb388187adc4c73d9dba24f1199a
kishanga <85829332+kishanga@users.noreply.github.com>
2023-12-04 21:29:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
-global model
-global conf
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration
+import av
+import cv2
 
-model = something
-conf = something_else
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-def _display_detected_frames( image, is_display_tracking=None, tracker=None):
-    global conf
-    global model
-    image = cv2.resize(image, (720, int(720*(9/16))))
-    if is_display_tracking:
-        res = model.track(image, conf=conf, persist=True, tracker=tracker)
-    else:
-        # Predict the objects in the image using the YOLOv8 model
-        res = model.predict(image, conf=conf)
-    # Return the processed frame to callback
-    res_plotted = res[0].plot()
-    return res_plotted
+class VideoProcessor:
+	def recv(self, frame):
+		frm = frame.to_ndarray(format="bgr24")
 
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    is_display_tracker, tracker = display_tracker_options()
-    image = _display_detected_frames(img, is_display_tracker, tracker)
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
+		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
 
-webrtc_streamer(
-    key="example",
-    video_frame_callback=callback,
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-)
+		for x,y,w,h in faces:
+			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+
+		return av.VideoFrame.from_ndarray(frm, format='bgr24')
+
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
+				rtc_configuration=RTCConfiguration(
+					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+					)
+	)
