5a57168ecc53300e8ef75f4f8c6efac30763d8e0
kishanga <85829332+kishanga@users.noreply.github.com>
2023-12-04 23:26:04 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -134,10 +134,10 @@ class VideoProcessor:
 			    num_coords = len(results.pose_landmarks.landmark)
 			    landmarks = ['class']
 			    for val in range(1, num_coords+1):
-				landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
+				    landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
 			    with open('Prediction_info.csv', mode='w', newline='') as f:
-				csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-				csv_writer.writerow(landmarks)
+				    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+				    csv_writer.writerow(landmarks)
 			    class_name = 'abc'
 			except:
 			    pass
